0592a89829eecfaa3f88cd99f148d63b43f9f3ad
Aminata Ba <blackandready1234@gmail.com>
2025-06-19 18:32:43 +0200

    Update page.js to fit the styling

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,15 +2,17 @@ import styles from './SignupPage.module.css';
 import RoleSelector from '../components/signup/RoleSelector';
 export default function SignupPage() {
   return (
-    <main className="home">
-      <h1 className="hero-title">Get started</h1>
-      <p className="hero-subtitle">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-      </p>
+   <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Get started</h1>
+        <p className={styles.subtitle}>
+          Lorem ipsum dolor sit amet consectetur. Diam amet sed eget in magna lacus.
+        </p>
 
-      <div className="card">
+        <div className={styles.card}> 
         <RoleSelector></RoleSelector>
       </div>
+      <button className={styles.button}>Sign up →</button>
     </main>
   );
 }
