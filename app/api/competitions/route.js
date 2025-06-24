3d957e3ad6b7de7e3352aff06e4b3637e95a1991
andrpavl <puvlo@yahoo.com>
2025-06-25 00:01:25 +0200

    added routes for competitions (GET, POST, GET by id). Added JWT verification in creating of competition

---
+++ app/api/competitions/route.js
@@ -0,0 +1,27 @@
+import dbConnect from '@/utils/dbConnects';
+import Competition from '../models/Competition';
+import { verifyToken } from '@/utils/auth';
+
+export async function GET() {
+  await dbConnect();
+  const competitions = await Competition.find().sort({ createdAt: -1 });
+  return Response.json(competitions);
+}
+
+export async function POST(req) {
+  try {
+    await dbConnect();
+    verifyToken(req);
+
+    const competitionData = await req.json();
+
+    const created = await Competition.create(competitionData);
+    return Response.json(created, { status: 201 });
+  } catch (error) {
+    console.error('Error creating competition:', error);
+    return Response.json(
+      { error: 'Failed to create competition' },
+      { status: 401 },
+    );
+  }
+}
