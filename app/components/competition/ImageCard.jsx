077b9429b38376d88c5142f0bd79d85fd3501154
yuusuf <yuusuf98@outlook.com>
2025-06-26 23:27:34 +0200

    add static image card with no functionality yet.

--- app/components/competition/ImageCard.jsx
+++ app/components/competition/ImageCard.jsx
@@ -1,3 +1,25 @@
-export default function ImageCard() {
-  return <></>;
-}
+import styles from '../../competition-gallery/competition-gallery.module.css';
+
+
+const ImageCard = ({imageUrl}) => {
+  return (
+    <div className={styles.card}>
+      <img src={imageUrl} alt="Competition" className={styles.image} />
+
+      <div className={styles.overlay}>
+        <div className={styles.buttonGroup}>
+          <div className={styles.button}>
+            <span className={styles.icon}>♡</span>
+            <span>Stem</span>
+          </div>
+          <div className={styles.button}>
+            <span className={styles.icon}>⏴</span>
+            <span>Del</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
