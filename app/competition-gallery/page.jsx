7a5d1d512f25774302b0b939f89233768f321b86
yuusuf <yuusuf98@outlook.com>
2025-06-26 23:30:54 +0200

    Add CompetitionGalleryPage

---
+++ app/competition-gallery/page.jsx
@@ -0,0 +1,10 @@
+import CompetitionGallery from '../components/competition/CompetitionGallery';
+import JoinButton from '../components/competition/JoinButton';
+export default function CompetitionGalleryPage() {
+  return (
+    <>
+      <CompetitionGallery />
+      <JoinButton />
+    </>
+  );
+}
