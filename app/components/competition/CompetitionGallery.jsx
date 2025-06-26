52b653bebb984ef59ae557c0c140c0a992dabff4
yuusuf <yuusuf98@outlook.com>
2025-06-26 23:29:28 +0200

    Add competition gallery component that maps over static images for now.

--- app/components/competition/CompetitionGallery.jsx
+++ app/components/competition/CompetitionGallery.jsx
@@ -1,3 +1,15 @@
+import ImageCard from './ImageCard';
+import styles from '../../competition-gallery/competition-gallery.module.css';
+
 export default function CompetitionGallery() {
-  return <></>;
+  const images = Array(30).fill('default-image.png');
+  return (
+    <>
+      <div className={styles.gallery}>
+        {images.map((imgUrl, index) => (
+          <ImageCard key={index} imageUrl={imgUrl} />
+        ))}
+      </div>
+    </>
+  );
 }
