be96ea8c25ea92ed68dcd1cdf59bd6195a6afa81
gayathri <gayathri.cake@gmail.com>
2025-06-27 11:11:52 +0200

    add entry submission form ui

---
+++ app/participant-entry/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+import EntryForm from "../components/participant/entry-form";
+
+export default function ParticipantEntryPage() {
+  return (
+    <div>
+      <EntryForm />
+    </div>
+  );
+}
