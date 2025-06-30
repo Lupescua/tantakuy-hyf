6367f633bc87d9e0d51c6155ef4fc1b5561a26a9
gayathri <gayathri.cake@gmail.com>
2025-06-30 11:17:11 +0200

    Remove duplicate declaration

--- app/participant-entry/page.jsx
+++ app/participant-entry/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 import EntryForm from '../components/participant/entry-form';
 import NavbarLoggedIn from '../components/layouts/NavbarLoggedIn/NavbarLoggedIn';
-
 export default function ParticipantEntryPage() {
   return (
     <div>
