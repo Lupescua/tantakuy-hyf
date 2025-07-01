7eb542aaf5cd03027b33ee5abc97f19a1cec8b8e
Soheib Ider <idersoheib@gmail.com>
2025-07-02 00:37:15 +0200

    remove navbar from participant-entry page

--- app/participant-entry/page.jsx
+++ app/participant-entry/page.jsx
@@ -1,10 +1,8 @@
 'use client';
 import EntryForm from '../components/participant/entry-form';
-import NavbarLoggedIn from '../components/layouts/NavbarLoggedIn/NavbarLoggedIn';
 export default function ParticipantEntryPage() {
   return (
     <div>
-      <NavbarLoggedIn />
       <EntryForm />
     </div>
   );
