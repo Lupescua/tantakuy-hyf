af48f42f382469bf0d733fe213abc319fe40c762
Stefany <Stefanyromerov88@gmail.com>
2025-07-02 16:51:12 +0200

    Removed the footer from all pages and kept it only on the homepage — still wondering if we really need this footer?

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,6 @@ export default async function RootLayout({ children }) {
       <body className={`${montserrat.variable} antialiased`}>
         {isLoggedIn ? <NavbarLoggedIn /> : <NavbarLoggedOut />}
         {children}
-        {isLoggedIn ? <FooterLoggedIn /> : <FooterLoggedOut />}
       </body>
     </html>
   );
