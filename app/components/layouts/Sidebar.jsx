99c691315db569078540df4e49b15c36427f5791
Stefany <Stefanyromerov88@gmail.com>
2025-07-04 13:53:13 +0200

    Added the icons inside the sidebar and the green color

--- app/components/layouts/Sidebar.jsx
+++ app/components/layouts/Sidebar.jsx
@@ -1,5 +1,6 @@
 'use client';
 import styles from './Sidebar.module.css';
+import { FaRegBuilding, FaBars, FaThLarge, FaRegUser } from 'react-icons/fa';
 
 export default function Sidebar({ open, setOpen }) {
   return (
@@ -13,13 +14,13 @@ export default function Sidebar({ open, setOpen }) {
               <a className={styles.sidebarButton} href="/company/profile">Virksomhedsoplysninger</a>
             </li>
             <li>
-              <a className={styles.sidebarButton} href="/company/competitions">Mine konkurrencer</a>
+              <a className={styles.sidebarButton} href="/company/competitions"><FaBars style={{marginRight: '8px'}} />Mine konkurrencer</a>
             </li>
             <li>
-              <a className={styles.sidebarButton} href="/company/dashboard">Kontrolpanel</a>
+              <a className={styles.sidebarButton} href="/company/dashboard"><FaThLarge style={{marginRight: '8px'}} />Dashboard</a>
             </li>
             <li>
-              <a className={styles.sidebarButton} href="/company/settings">Indstillinger</a>
+              <a className={styles.sidebarButton} href="/company/settings"><FaRegUser style={{marginRight: '8px'}} />Indstillinger</a>
             </li>
           </ul>
         </nav>
