6dcf2e898ccd4bbbbf6ae50077f72d71c2584c8e
Aminata Ba <blackandready1234@gmail.com>
2025-07-04 02:10:26 +0200

    Update FooterLoggedOut.jsx
    the sign up and log in buttons are not necessary on the footer. we agreed on this and discussed it therefore I am automatically merging

--- app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
+++ app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
@@ -5,21 +5,7 @@ export default function FooterLoggedOut() {
   return (
     <footer className={styles.footer}>
       <div className={styles['footer-content']}>
-        <p className={styles['footer-text']}>¿Do you have an account?</p>
-        <div className={styles['footer-auth-buttons']}>
-          <Link
-            href="/login"
-            className={`${styles['footer-btn']} ${styles['login-btn']}`}
-          >
-            Log in
-          </Link>
-          <Link
-            href="/signup"
-            className={`${styles['footer-btn']} ${styles['signup-btn']}`}
-          >
-            Sign up
-          </Link>
-        </div>
+        <p className={styles['footer-text']}>Do you have an account?</p>
       </div>
     </footer>
   );
