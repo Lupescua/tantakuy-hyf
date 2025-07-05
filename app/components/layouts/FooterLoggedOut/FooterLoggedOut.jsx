126cf0caecf0d03d27b8d2a38ea1ffa6e9229e4c
Aminata Ba <blackandready1234@gmail.com>
2025-07-05 23:25:02 +0200

    Update FooterLoggedOut.jsx

--- app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
+++ app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
@@ -1,11 +1,24 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './FooterLoggedOut.module.css';
 
 export default function FooterLoggedOut() {
+  const pathname = usePathname();
+
+  // one can add the paths  to hide in this footer
+  const hiddenFooterPaths = ['/login', '/signup'];
+
+
+  if (hiddenFooterPaths.includes(pathname)) {
+    return null;
+  }
+
   return (
     <footer className={styles.footer}>
       <div className={styles['footer-content']}>
-        <p className={styles['footer-text']}>Do you have an account?</p>
+        <p className={styles['footer-text']}> Do you have an account? </p>
         <div className={styles['footer-auth-buttons']}>
           <Link
             href="/login"
