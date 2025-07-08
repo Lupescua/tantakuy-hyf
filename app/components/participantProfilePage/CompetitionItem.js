e8a3f30ade26bfcfb9e06edb158d2325172cdd3a
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-07-08 18:36:05 +0200

    Refactor CompetitionItem component to include image display

--- app/components/participantProfilePage/CompetitionItem.js
+++ app/components/participantProfilePage/CompetitionItem.js
@@ -1,6 +1,6 @@
 import styles from './CompetitionItem.module.css';
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function CompetitionItem({
   id,
   title,
@@ -8,12 +8,23 @@ export default function CompetitionItem({
   likes,
   shares,
   saved,
+  imageUrl,
 }) {
   return (
     <Link href={`/competition/${id}`} className={styles.competitionItem}>
       <div className={styles.infoLeft}>
-        <h4 className={styles.title}>{title}</h4>
-        <span className={styles.organizer}>{organizer}</span>
+        <div className={styles.header}>
+          <h4 className={styles.title}>{title}</h4>
+          <span className={styles.separator}> - </span>
+          <span className={styles.organizer}>{organizer}</span>
+        </div>
+        <Image
+          src={imageUrl}
+          alt="Your submission"
+          width={93}
+          height={57}
+          className={styles.userImage}
+        />
       </div>
       <div className={styles.stats}>
         <span className={`${styles.statItem} ${styles.like}`}>
