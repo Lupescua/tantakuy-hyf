841ea14f07c56353a02c6fa1094433ee7fba1b38
andrpavl <puvlo@yahoo.com>
2025-07-09 13:43:16 +0200

    Update according requirements; Add axios, Loader and Next error handling; Add verification

--- app/participant/[id]/profile/page.jsx
+++ app/participant/[id]/profile/page.jsx
@@ -3,7 +3,6 @@ import UserProfileSidebar from '@/app/components/participantProfilePage/Sidebar'
 import ProfileSection from '@/app/components/participantProfilePage/ProfileSection';
 import NotificationsPanel from '@/app/components/participantProfilePage/NotificationPanel';
 import styles from './page.module.css';
-import { useState } from 'react';
 
 export default function UserProfile() {
   return (
