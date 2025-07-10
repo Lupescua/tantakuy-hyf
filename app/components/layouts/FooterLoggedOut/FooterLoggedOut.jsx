5071c4220c5a07834bd3d939ffca8b1d2c60c698
Soheib Ider <idersoheib@gmail.com>
2025-07-10 12:58:25 +0200

    bulk format fix

--- app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
+++ app/components/layouts/FooterLoggedOut/FooterLoggedOut.jsx
@@ -6,13 +6,12 @@ export default function FooterLoggedOut() {
   return (
     <footer className={styles.footer}>
       <div className={styles['footer-content']}>
-       <p className={styles['footer-text']}>
+        <p className={styles['footer-text']}>
           Har du en konto?{' '}
           <Link href="/login" className={styles['login-link']}>
             Se mere
           </Link>
         </p>
-        
       </div>
     </footer>
   );
