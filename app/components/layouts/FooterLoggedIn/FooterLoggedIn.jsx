116d1e48ac38bf896075b8e57c28c742273aff2f
yuusuf <yuusuf98@outlook.com>
2025-07-11 14:53:46 +0200

    Make desktop and mobile search work together and have same functionality using shared state context.

--- app/components/layouts/FooterLoggedIn/FooterLoggedIn.jsx
+++ app/components/layouts/FooterLoggedIn/FooterLoggedIn.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { FiInstagram, FiHome, FiSearch, FiHeart, FiUser } from 'react-icons/fi';
 import styles from './FooterLoggedIn.module.css';
+import { useMobileSearch } from '../../../../context/MobileSearchContext';
 
 export default function Footer() {
+  const { toggleMobileSearch } = useMobileSearch();
   return (
     <footer className={styles.footer}>
       {/* footer desktop */}
@@ -27,7 +29,7 @@ export default function Footer() {
         <Link href="/" aria-label="Home" className={styles['footer-icon']}>
           <FiHome />
         </Link>
-        <button className={styles['footer-icon']} aria-label="Search">
+        <button className={styles['footer-icon']} aria-label="Search" onClick={toggleMobileSearch}>
           <FiSearch />
         </button>
         <button className={styles['footer-icon']} aria-label="Likes">
