ee30fd9dc79006c02e44e349436a7189fa85222a
yuusuf <yuusuf98@outlook.com>
2025-07-11 14:53:46 +0200

    Make desktop and mobile search work together and have same functionality using shared state context.

---
+++ context/MobileSearchContext.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { createContext, useContext, useState } from 'react';
+
+const MobileSearchContext = createContext();
+
+export function MobileSearchProvider({ children }) {
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+
+  const toggleMobileSearch = () => {
+    setShowMobileSearch((prev) => !prev);
+  };
+
+  return (
+    <MobileSearchContext.Provider value={{ showMobileSearch, toggleMobileSearch }}>
+      {children}
+    </MobileSearchContext.Provider>
+  );
+}
+
+// Hook to use in components
+export function useMobileSearch() {
+  return useContext(MobileSearchContext);
+}
