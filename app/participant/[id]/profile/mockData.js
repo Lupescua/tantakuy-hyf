cc6dbace721934886ac992113c575db02262609a
andrpavl <puvlo@yahoo.com>
2025-07-12 13:29:14 +0200

    Restore deleted file app/participant/[id]/profile/mockData.js

---
+++ app/participant/[id]/profile/mockData.js
@@ -0,0 +1,34 @@
+export const mockUser = {
+  firstName: 'Peter',
+  lastName: 'Plys',
+  region: 'Midtjylland',
+  about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+};
+
+export const mockCompetitions = [
+  {
+    id: 1,
+    title: 'Billigblomst - Danmarks Vildeste have',
+    organizer: 'Billigblomst',
+    likes: 100,
+    shares: 5,
+    saved: 8,
+  },
+  {
+    id: 2,
+    title: 'Søstrene Grene - Upcycle Julepynt',
+    organizer: 'Søstrene Grene',
+    likes: 20,
+    shares: null,
+    saved: 5,
+  },
+  {
+    id: 3,
+    title: 'Jem & Fix - DIY Upcycle old furniture',
+    organizer: 'Jem & Fix',
+    likes: 30,
+    shares: 2,
+    saved: 7,
+  },
+];
